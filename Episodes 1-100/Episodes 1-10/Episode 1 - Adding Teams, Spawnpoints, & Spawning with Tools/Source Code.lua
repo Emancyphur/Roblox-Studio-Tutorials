20207fba7318b7c0e4f416734ba67c997e9eb805
Emancyphur <40709590+Emancyphur@users.noreply.github.com>
2022-03-22 04:00:10 -0700

    Update and rename Episodes 1-100/Episodes 1-10/Episode 1 - Custom Teams, Team-Only Gear, Custom Clothing, & More/Source Code.lua to Episodes 1-100/Episodes 1-10/Episode 1 - Adding Teams, Spawnpoints, & Spawning with Tools/Source Code.lua

---
+++ Episodes 1-100/Episodes 1-10/Episode 1 - Adding Teams, Spawnpoints, & Spawning with Tools/Source Code.lua
@@ -0,0 +1,6 @@
+-- No scripts were provided for Episode 1 of the Roblox Studio Tutorial Series.
+
+-- The only code that was used during the video is related to adding a gear into your game from the Avatar Shop using InsertService
+game:GetService("InsertService"):LoadAsset(1).Parent = workspace
+--[[ The number 1 is replaced with the ID of an item in Roblox's Avatar Shop (the number in the web address bar)
+and the code is input to the command bar in Roblox Studio --]]
