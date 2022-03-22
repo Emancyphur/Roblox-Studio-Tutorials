4e47977ef84594aa4ffd5c0beb836a1956cdd2c0
Emancyphur <40709590+Emancyphur@users.noreply.github.com>
2022-03-22 04:01:14 -0700

    Update Source Code.lua

--- Episodes 1-100/Episodes 1-10/Episode 1 - Adding Teams, Spawnpoints, & Spawning with Tools/Source Code.lua
+++ Episodes 1-100/Episodes 1-10/Episode 1 - Adding Teams, Spawnpoints, & Spawning with Tools/Source Code.lua
@@ -1,6 +1,6 @@
 -- No scripts were provided for Episode 1 of the Roblox Studio Tutorial Series.
 
--- The only code that was used during the video is related to adding a gear into your game from the Avatar Shop using InsertService
+-- The only code that was used during the video is related to adding a gear into your game from the Avatar Shop using the InsertService
 game:GetService("InsertService"):LoadAsset(1).Parent = workspace
 --[[ The number 1 is replaced with the ID of an item in Roblox's Avatar Shop (the number in the web address bar)
-and the code is input to the command bar in Roblox Studio --]]
+and the code is input to the command bar in Roblox Studio. After clicking enter, the item will be added to the Workspace, contained in a Model --]]
